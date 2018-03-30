d5527169e63907e682a38a73e830ae0ff42b4db3
eyi1 <ej7729@gmail.com>
2018-03-30 21:48:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
+def prime? (num)
+return false if num < 2
+  arr = (2...num).to_a 
+  arr.each do |x|
+    if num / x != 0
+      true 
+    else 
+      false 
+    end
+  end
+
+end
 # Add  code here!
